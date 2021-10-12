20ed3d791bdeed34e87f31518ba1081d8397b633
jyoti857 <57531083+jyoti857@users.noreply.github.com>
2021-10-13 00:36:00 +0530

    hide the necessary api details

--- src/firebase/firebase-utils.js
+++ src/firebase/firebase-utils.js
@@ -3,14 +3,14 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const config = {
-        apiKey: "AIzaSyBQubA4IX-uI5_HhebsA3LdcBVm_sXLn5I",
+        apiKey: "",
         authDomain: "crown-db-3fb64.firebaseapp.com",
         databaseURL: "https://crown-db-3fb64.firebaseio.com",
         projectId: "crown-db-3fb64",
         storageBucket: "crown-db-3fb64.appspot.com",
         messagingSenderId: "191270543034",
-        appId: "1:191270543034:web:b02c94eeab820c97d22c83",
-        measurementId: "G-W7HRW4MYQD"
+        appId: "",
+        measurementId: ""
     };
 
 export const createUserProfileDocument = async(userAuth, additionalData) =>{
